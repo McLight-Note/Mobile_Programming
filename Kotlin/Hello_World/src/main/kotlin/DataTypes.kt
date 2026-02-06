1e17e4108e0e85fdcfd99872de98719ce09ceb8c
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2026-02-06 19:19:46 +0900

    First Steps on Mobile Programming

---
+++ Kotlin/Hello_World/src/main/kotlin/DataTypes.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val name: String = "Muhammad"
+    val age: Int = 24
+    println("$name is $age years old")
+
+    val number = 23
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+    println("Integer Max Value is $integerMaxValue")
+    println("Integer Minimum Value is $integerMinValue")
+    
+}
---
+++ Kotlin/Hello_World/src/main/kotlin/Variables.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var userName: String = "Muhammad"
+    var age: Int = 24
+    println("Hi $userName")
+    println("You are $age years old")
+}
