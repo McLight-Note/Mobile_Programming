3ee040d373756b3fbc16ec8278564cc9c2208164
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2026-04-07 11:59:04 +0900

    Presentation for 1-2 classes

---
+++ 6-lesson/presentation/ppt.java
@@ -0,0 +1,18 @@
+public class FloatVsDouble {
+    public static void main(String[] args) {
+        // Float
+        float a = 56.2F, b = 12.8F;
+        System.out.println("Float addition:    " + (a + b));
+
+        float g = 12.4F, u = 1.8F;
+        System.out.println("Float subtraction: " + (g - u));
+
+        // Double
+        double l = 2.2, k = 1.9;
+        System.out.println("Double multiply:   " + (l * k));
+
+        double ww = 6.2, dd = 2.1;
+        System.out.println("Double division:   " + (ww / dd));
+    }
+}
+
